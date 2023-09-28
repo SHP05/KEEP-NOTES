72f7f2cfb571d96aef60884a39a6e505d976abb2
Suman Pankhaniya <suman.h.pankhaniya555@gmail.com>
2023-09-28 12:14:58 +0530

    Add Loading effect in home page

--- Client/src/Componenets/Home.jsx
+++ Client/src/Componenets/Home.jsx
@@ -1,17 +1,32 @@
 import { useEffect, useState } from "react";
 import Create from "./Create";
 import axios from "axios";
+
 const Home = () =>{
     const [todos,setTodos] = useState([]);
+
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+      }, 3000);
+    }, []);
+  
+
     useEffect(()=>{
         axios.get('https://task-manager-mern-app.vercel.app/get')
-        .then(result => setTodos(result.data))
+        .then(result => {
+            setTodos(result.data)
+            setLoading(false)
+        })
         .catch(err => console.log(err))
     },[])
 
     const hendleEdit = async (id) =>{
         await axios.put('https://task-manager-mern-app.vercel.app/update/'+id)
-        .then(result => setTodos(result))
+        .then(result => {setTodos(result)})
         .catch(err => console.log(err))
     }
 
@@ -20,19 +35,32 @@ const Home = () =>{
         <div>
             <h1>Todo List</h1>
             <Create />
-
             { 
-                todos.length === 0 ? (<h1>No Todo</h1>)
-                : todos.map((todo,key) => (
+             loading ? (
+                <div className="loader-container">
+                  {/* <div className="spinner"></div> */}
+                  <div className="custom-loader"></div>
+                </div>
+              ) : (
+                todos.map((todo,key) => (
                 <div key={key} className="mostly-customized-scrollbar">
                     <span>{todo.title}</span>
                     <h3>{todo.task}</h3>
                     <button onClick={() =>hendleEdit(todo._id)}>Edit</button>
                 </div>
-                ))
+                )))
             }
         </div>
         </>
     )
 }
 export default Home;
+
+{/* todos.length === 0 ? (<h1>No Todo</h1>)
+                : todos.map((todo,key) => (
+                <div key={key} className="mostly-customized-scrollbar">
+                    <span>{todo.title}</span>
+                    <h3>{todo.task}</h3>
+                    <button onClick={() =>hendleEdit(todo._id)}>Edit</button>
+                </div>
+                )) */}
